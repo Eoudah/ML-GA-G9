ea79bc3302b17adf26e6d0584ed655a1ee1fe7ee
lucialafee <lucia.lafee@student.ie.edu>
2025-02-10 11:35:05 +0100

    Update Streamlit-G9-V1.py

--- Streamlit-G9-V1.py
+++ Streamlit-G9-V1.py
@@ -26,7 +26,7 @@ st.markdown("""
 	- **Species:** The species of the penguin (Adelie, Chinstrap, Gentoo).
 	- **Bill Length (mm):** The length of the penguin’s bill (in mm).
 	- **Flipper Length (mm):** The length of the penguin’s flipper (in mm).
- 	- **Sex:** The sex of the penguin (Male, Female).
+ - **Sex:** The sex of the penguin (Male, Female).
 	- **Body Mass (g):** Value we are predicting. The body mass of the penguin (in grams).
 """)
 
