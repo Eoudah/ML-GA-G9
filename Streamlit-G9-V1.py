b9a4067b3de46c234eb72c6505fb308c09ea4022
lucialafee <lucia.lafee@student.ie.edu>
2025-02-10 11:52:38 +0100

    Update Streamlit-G9-V1.py

--- Streamlit-G9-V1.py
+++ Streamlit-G9-V1.py
@@ -5,6 +5,15 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
+st.markdown("""
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
 st.title("Penguin Body Mass Predictor")
 
 st.image('./AI PENGUIN 2.png', caption='Picture made with AI', width=500)
